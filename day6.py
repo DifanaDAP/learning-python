364a51a0604684627b519f394e4cb92b79f312b1
DifanaDAP <148295120+DifanaDAP@users.noreply.github.com>
2025-03-10 15:15:02 +0700

    new file:   day6.py

---
+++ day6.py
@@ -0,0 +1,10 @@
+# Day 6 : Dictionary & set python
+
+# Dictionary - Struktur data key value
+mahasiswa+{
+    "nama": "Didit"
+    "umur": 22
+    "jurusan": "Elektro"
+}
+
+# Akses nilai dalam dictionary
