b626203ce063aee2bfe580216cc5b523e33ef80b
DifanaDAP <148295120+DifanaDAP@users.noreply.github.com>
2025-03-20 22:54:02 +0700

    modified:   day9.py

--- day9.py
+++ day9.py
@@ -1,46 +1,65 @@
-# Day 9 Lambda function, map, and filter dalam python
+# Day 9 Exception Handling dalam python
 
-# Lambda Fuction - Fungis anonom dalam satu baris
-kuadrat = lambda X : X ** 2
-print("kuadrat dari 4:", kuadrat(4))
+# Contoh dasar exception handling
+try:
+    angka = int(input("Masukkan sebuah angka: "))
+    hasil = 10 / angka
+    print("Hasil bagi 10 dengan angka tersebut adalah:", hasil)
+except ZeroDivisionError:
+    print("Error: Tidak bisa membagi dengan nol!")
+except ValueError:
+    print("Error: Harap masukkan angka yang valid!")
+except Exception as e:
+    print(f"Error tidak terduga: {e}")
 
-penjumlahan = lambda a, b : a + b
-print("Hasil penjumlahan 5 + 4:", penjumlahan(5, 4))
+# menggunakan finally
+try:
+    file = open("data.txt", "r")
+    print(file.read())
+except FileNotFoundError:
+    print("Error: File tidak ditemukan!")
+finally:
+    print("Eksekusi blok finally, apakah ada error atau tidak.")
 
-# mengunakan lambda dalam fungsi bawaan python
-angka = [1, 2, 3, 4, 5]
-kuadrat_angka = list(map(lambda x : x ** 2, angka))
-print("list angka dikuadratkan:", kuadrat_angka)
+# menggunakan raise untuk membuat error sendiri
+def cek_umur(umur):
+    if umur < 0 :
+        raise ValueError("Umur tidak bisa negatif!")
+    print("Umur anda: ", umur)
 
-# Menggunakan filter untuk menyaring data
-angka_genap = list(filter(lambda x : x % 2 == 0, angka))
-print("Angka genap dari list", angka_genap)
-
-# Menggunakan lambda dalam sorting
-mahasiswa = [("Alice", 22), ("Bob", 20), ("charlie", 23)]
-mahasiswa.sort(key = lambda x : x[1]) # sort berdasarkan umur
-print("Mahasiswa setelah diurutkan berdasarkan umur : ", mahasiswa)
+try:
+    cek_umur(int(input("Masukan Umur: ")))
+except ValueError as e:
+    print("Error:", e)
 
 # Latihan
-# 1. Buat lambda function untuk menghitung luas lingkaran dengan jari-jari sebagai parameter.
-# 2. Gunakan map() untuk mengubah list suhu dari Celcius ke Fahrenheit.
-# 3. Gunakan filter() untuk menyaring angka yang lebih besar dari 10 dalam sebuah list.
+# 1. Buat exception handling untuk menangani input string yang tidak bisa dikonversi ke float.
+print("Latihan 1")
+try:
+    angka = float(input("Masukkan angka desimal: "))
+    print("Angka yang dimasukkan:", angka)
+except ValueError:
+    print("Error: Harap masukkan angka yang valid!")
 
-# Latihan 1
-print("latihan 1")
-
-lingkaran = lambda x : (x ** 2) * 3.14
-print("luas lingkaran : ", lingkaran(7))
-
-# Latihan 2
+# 2. Tangani error jika pengguna memasukkan indeks yang melebihi panjang list.
 print("latihan 2")
-celsius = [0, 20, 30, 37, 100]
-farenheit = list(map(lambda c : (c * 9/5) + 32, celsius))
-print("Suhu Farenheit:", farenheit)
+data = [10, 20, 30, 40 , 50]
+try:
+    indeks = int(input("Masukan indeks(0-4): "))
+    print("Data pada indek tesebut:", data[indeks])
+except IndexError:
+    print("Error: Indeks di luar jangkauan!")
+except ValueError:
+    print("Error: Harap masukan angka yang valid")
+    
+# 3. Buat fungsi yang membaca file dan menangani jika file tidak ditemukan.
+print("Latihan 3")
+def baca_file(nama_file):
+    try:
+        with open(nama_file, "r") as file:
+            print(file.read())
+    except FileNotFoundError:
+        print(" Error: File tidak ditemukan!")
 
-# Latihan 3
-print("latihan 3")
-
-list_angka = [4, 3, 7, 9, 12, 18, 20]
-menyaring = list(filter(lambda x : x > 10, list_angka))
-print("Angka sesudah di saring", menyaring)
+#contoh pemangilan fungsi
+baca_file("data.txt")
